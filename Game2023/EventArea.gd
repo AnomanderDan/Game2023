67718fa9b383b33f1531afa70ebc90c7d80e9ab5
BURNSIDE HIGH SCHOOL <124309580+AnomanderDan@users.noreply.github.com>
2023-08-04 12:46:02 +1200

    event area

--- Game2023/EventArea.gd
+++ Game2023/EventArea.gd
@@ -1,6 +1,14 @@
 extends Area
 
 
+func _ready():
+	$AnimationPlayer.play("Nothing")
+
 func _on_EventArea_body_entered(body):
 	if body.name == "Player":
 		$AnimationPlayer.play("Event")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Event":
+		queue_free()
